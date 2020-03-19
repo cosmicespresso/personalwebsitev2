8e2bc35923a816f534271f260639bed44e54f401
Kallirroi <kallirroi.retzepi@gmail.com>
2020-03-19 17:15:29 -0400

    location + animation

--- src/App.js
+++ src/App.js
@@ -8,26 +8,30 @@ function App() {
   ReactGA.initialize('UA-89607558-1', { standardImplementation: true });
 
   return (
-    <Draggable>
-    <div className="App">
-      <img className='bar' src={bar} alt='bar'/>
-      <div className='main'>
-        <p className='text'>
-        I use code and design to re-imagine the Web. CV and Portfolio upon request.
-        </p>
+    <div className='wrapper'>
+      <Draggable>
+        <div className="App">
+          <img className='bar' src={bar} alt='bar'/>
+          <div className='main'>
+            <p className='text'>
+            I use code and design to re-imagine the Web. CV and Portfolio upon request.
+            </p>
 
-        <p className='currently'>CURRENTLY  &#x2192; <a href="https://www.recurse.com">Recurse Center</a>, adj. faculty SVA DSI, <a href="https://foreignobjects.net/" target="_blank" rel="noopener noreferrer"> FOREIGN OBJECTS</a>. <br/>
-          PREVIOUSLY &#x2192; MIT Media Lab.
-        </p>
+            <p className='currently'>CURRENTLY  &#x2192; <a href="https://www.recurse.com">Recurse Center</a>, adj. faculty SVA DSI, <a href="https://foreignobjects.net/" target="_blank" rel="noopener noreferrer"> FOREIGN OBJECTS</a>. <br/>
+              PREVIOUSLY &#x2192; MIT Media Lab.
+            </p>
 
-        <p className='links'> 
-          <a href="mailto:kalli.retzepi@gmail.com" target="_blank" rel="noopener noreferrer">email</a> <br/>
-          <a href="https://www.are.na/kalli-retzepi/index" target="_blank" rel="noopener noreferrer">are.na</a> <br/>
-          <a href="https://twitter.com/kallirroi" target="_blank" rel="noopener noreferrer">twitter</a>
-        </p>
-      </div>
+            <p className='links'> 
+              <a href="mailto:kalli.retzepi@gmail.com" target="_blank" rel="noopener noreferrer">email</a> <br/>
+              <a href="https://www.are.na/kalli-retzepi/index" target="_blank" rel="noopener noreferrer">are.na</a> <br/>
+              <a href="https://twitter.com/kallirroi" target="_blank" rel="noopener noreferrer">twitter</a> <br/>
+              <a href="https://github.com/Kallirroi" target="_blank" rel="noopener noreferrer">github</a>
+            </p>
+          </div>
+        </div>
+      </Draggable>
+      <div className='location'>currently (stuck) in Brooklyn</div>
     </div>
-    </Draggable>
   );
 }
 
